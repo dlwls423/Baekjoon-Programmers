6937b2b907c622db45522a77433e2bdde869ce43
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-10-22 15:20:37 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.45 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        StringBuilder answer = new StringBuilder();
+        int L = -1;
+        int R = -1;
+        int[][] keypad = {{1, 4, 7, -1}, {2, 5, 8, 0}, {3, 6, 9, -1}};
+        int[] left = {0, 3};
+        int[] right = {2, 3};
+        int[] now = new int[2];
+        
+        for (int number : numbers) {
+            char c = '0';
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (number == keypad[j][k]) {
+                        now[0] = j;
+                        now[1] = k;
+                        break;
+                    }
+                }
+            }
+            if (now[0] == 0) c = 'L';
+            else if (now[0] == 2) c = 'R';
+            else {
+                int Ldistance = Math.abs(now[0] - left[0]) + Math.abs(now[1] - left[1]);
+                int Rdistance = Math.abs(now[0] - right[0]) + Math.abs(now[1] - right[1]);
+                if (Ldistance == Rdistance) c = (char)(hand.charAt(0)-'a'+'A');
+                else if (Ldistance < Rdistance) c = 'L';
+                else c = 'R';
+            }
+
+            answer.append(c);
+            if (c == 'L') {
+                left[0] = now[0];
+                left[1] = now[1];
+            } else {
+                right[0] = now[0];
+                right[1] = now[1];
+            }
+        }
+        return answer.toString();
+    }
+}

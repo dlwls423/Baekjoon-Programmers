991c060938004b97d25eb1269893c4ecf372241b
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-10-22 14:17:59 +0900

    [level 1] Title: 둘만의 암호, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        int[] alpha = new int[26];
+        for(int i=0;i<skip.length();i++) alpha[skip.charAt(i)-'a']++;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            int j=c-'a';
+            int num = 0;
+            while(num<index){
+                j = (j+1)%26;
+                if(alpha[j]!=1) num++;
+            }
+            answer.append((char) (j + 'a'));
+        }
+        return answer.toString();
+    }
+}

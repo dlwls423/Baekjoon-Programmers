09347bad66ac555bfd43194f70b5acf2ff26073b
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-10-26 19:26:12 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==1 && i>=idx) return i;
+        }
+        return answer;
+    }
+}

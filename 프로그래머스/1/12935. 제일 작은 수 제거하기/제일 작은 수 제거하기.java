f36fb3de635bc736b247cd538b915d509930b48c
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 12:38:10 +0900

    Update 제일 작은 수 제거하기.java

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] solution(int[] arr) {
         int len = arr.length;
-        int ans_len = len -1;
-        if(ans_len <= 0) ans_len = 1;
-        int[] answer = new int[ans_len];
-        if(ans_len == 1){
-            answer[0] = -1;
-            return answer;
-        }
-        
+        if(len == 1) return new int[] {-1};
+        int[] answer = new int[len-1];
+
         int min = arr[0];
         for(int i=1;i<len;i++){
             if(min > arr[i]) min = arr[i];
         }
+        
         int index=0;
         for(int i=0;i<len;i++){
             if(min != arr[i]) answer[index++] = arr[i];

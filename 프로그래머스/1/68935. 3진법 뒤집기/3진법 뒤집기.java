9635840d5d967629d4b2e37a51da9640ecb6df06
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 14:28:28 +0900

    Update 3진법 뒤집기.java

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,19 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        StringBuilder str = new StringBuilder();
-        while(n>=3){
-            str.append(Integer.toString(n%3));;
-            n /= 3;
-        }
-        str.append(n);
-        
-        int a = 1;
-        for(int i=str.length()-1;i>=0;i--){
-            answer += a*(str.charAt(i)-'0');
-            a *= 3;
-        }
-
-        return answer;
+        StringBuilder str = new StringBuilder(Integer.toString(n, 3));
+        str.reverse();
+        return Integer.parseInt(str.toString(), 3);
     }
 }

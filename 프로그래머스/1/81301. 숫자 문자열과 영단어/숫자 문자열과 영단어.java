8cbfac344b400c78f5e562dd5e1876b82ca25bd2
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 15:30:38 +0900

    Update 숫자 문자열과 영단어.java

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,27 @@
 class Solution {
     public int solution(String s) {
+        int answer = 0;
         String[] table = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i=0;i<10;i++){
-            s = s.replaceAll(table[i], Integer.toString(i));
+        int len = s.length();
+        for(int i=0;i<len;){
+            char c=s.charAt(i);
+            if(c >= '0' && c <= '9'){
+                answer = answer*10 + c - '0';
+                i++;
+            } 
+            else{
+                String str="";
+                if(i+5 > len) str = s.substring(i, len);
+                else str = s.substring(i, i+5);
+                for(int j=0;j<10;j++){
+                    if(str.contains(table[j])){
+                        i += table[j].length();
+                        answer = answer*10 + j;
+                        break;
+                    }
+                }
+            }
         }
-        return Integer.parseInt(s);
+        return answer;
     }
 }

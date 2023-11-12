3dbecada614befe00213612b2a253916601eaa62
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 18:15:57 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.39 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
-        int[] alpha = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+        int[] alpha = new int[26];
+        
         for (String key : keymap){
             for(int i=0;i<key.length();i++){
                 int alphbet = key.charAt(i)-'A';
-                if(alpha[alphbet] == -1) alpha[alphbet] = i;
-                else if(alpha[alphbet] > i) alpha[alphbet] = i;
+                if(alpha[alphbet] == 0) alpha[alphbet] = i+1;
+                else if(alpha[alphbet] > i+1) alpha[alphbet] = i+1;
             }
         }
 
         for(int i=0;i<targets.length;i++){
             for(int j=0;j<targets[i].length();j++){
                 int a = alpha[targets[i].charAt(j)-'A'];
-                if(a==-1){
+                if(a==0){
                     answer[i] = -1;
                     break;
                 }
-                else answer[i] += a+1;
+                else answer[i] += a;
             }
         }
         return answer;

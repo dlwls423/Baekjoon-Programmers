30dde8a0ccc38a0c41b55e97159cb9a8b8ee1e9c
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 16:47:29 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,11 +2,11 @@ class Solution {
     public String solution(int[] food) {
         StringBuilder answer = new StringBuilder();
         for(int i=1;i<food.length;i++){
-            for(int j=0;j<food[i]/2;j++) answer.append(Integer.toString(i));
+            answer.append(Integer.toString(i).repeat(food[i]/2));
         }
         answer.append('0');
         for(int i=food.length-1;i>0;i--){
-            for(int j=0;j<food[i]/2;j++) answer.append(Integer.toString(i));
+            answer.append(Integer.toString(i).repeat(food[i]/2));
         }
         return answer.toString();
     }

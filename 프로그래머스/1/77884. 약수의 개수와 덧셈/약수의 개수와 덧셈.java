b22d7e37e163e3978cfdc2a52175f2b0a676339f
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 12:54:38 +0900

    Update 약수의 개수와 덧셈.java

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -14,9 +14,10 @@ class Solution {
     
     public static int findDivisorNum(int n){
         int num = 0;
-        for(int i=1; i<=n; i++){
-            if(n%i==0) num++;
+        for(int i=1; i<Math.sqrt(n); i++){
+            if(n%i==0) num+=2;
         }
+        if(Math.sqrt(n)%1.0==0) num++;
         return num;
     }
 }

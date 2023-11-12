b3ba769124daf5a1ffd2893279a792934e4b820c
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 17:01:09 +0900

    Update 2016년.java

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,16 +1,13 @@
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
         int[] days = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] week = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
         int daySum = 0;
-        for(int i=0;i<a;i++){
+        for(int i=1;i<a;i++){
             daySum += days[i];
         }
         daySum += b;
         
-        answer += week[daySum%7];
-        
-        return answer;
+        return week[daySum%7];
     }
 }

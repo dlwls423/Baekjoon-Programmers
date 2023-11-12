27727ee3bcaa04e87be4f47913110736782b65ca
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 13:31:20 +0900

    Update 최대공약수와 최소공배수.java

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] solution(int a, int b) {
         int[] answer = new int[2];
 
-          answer[0] = gcd(a,b);
+        answer[0] = gcd(a,b);
         answer[1] = (a*b)/answer[0];
+        
         return answer;
     }
 
-   public static int gcd(int p, int q)
-   {
-    if (q == 0) return p;
-    return gcd(q, p%q);
-   }
+    public static int gcd(int p, int q){
+        if (q == 0) return p;
+        return gcd(q, p%q);
+    }
 }

f22b343a4e76cb1865f36dcd231198dd13d62df7
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 17:48:41 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.61 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for(int i=1;i<=number;i++){
+            answer += returnPower(i, limit, power);
+        }
+        return answer;
+    }
+
+    public int returnPower(int n, int l, int p){
+        int divisorNum = 0;
+        for(int i=1;i<Math.sqrt(n);i++){
+            if(n%i==0) divisorNum += 2;
+        }
+        
+        if(Math.sqrt(n) % 1.0 == 0) divisorNum++;
+        
+        if(divisorNum>l) return p;
+
+        return divisorNum;
+    }
+}

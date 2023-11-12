b27df63f451c7149ddefd09cdf7fbefd27c8ad01
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 12:23:03 +0900

    Update 없는 숫자 더하기.java

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,6 +1,7 @@
 class Solution {
+    static final int TOTAL_SUM = 45;
     public int solution(int[] numbers) {
-        int answer = 45;
+        int answer = TOTAL_SUM;
         for(int i=0;i<numbers.length;i++){
             answer -= numbers[i];
         }

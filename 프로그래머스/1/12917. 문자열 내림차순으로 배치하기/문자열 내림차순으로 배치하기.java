23c42ed2b14008f61d96ef605c8a0f175d0c5f8f
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-12 13:05:35 +0900

    Update 문자열 내림차순으로 배치하기.java

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,11 +1,9 @@
 import java.util.Arrays;
 
-
 class Solution {
     public String solution(String s) {
         char[] char_arr = s.toCharArray();
         Arrays.sort(char_arr);
-        String answer = new StringBuilder(new String(char_arr)).reverse().toString();
-        return answer;
+        return new StringBuilder(new String(char_arr)).reverse().toString();
     }
 }

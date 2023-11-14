eb1846ec6935865068411b4742e6097e4e012d1c
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-11-14 10:04:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -6,16 +6,19 @@ class Solution {
         int[] answer = new int[len];
         HashMap<String, Integer> reported = new HashMap<>();
         HashMap<String, Set<String>> reporting = new HashMap<>();
+        
         for (String id : id_list) {
             reported.put(id, 0);
             Set<String> temp_list = new HashSet<>();
             reporting.put(id, temp_list);
         }
+        
         for(String rep : report){
             String[] a = rep.split(" ");
             if(!reporting.get(a[0]).contains(a[1])) reported.put(a[1], reported.get(a[1])+1);
             reporting.get(a[0]).add(a[1]);
         }
+        
         int i=0;
         for(String id : id_list){
             for (String s : reporting.get(id)) {

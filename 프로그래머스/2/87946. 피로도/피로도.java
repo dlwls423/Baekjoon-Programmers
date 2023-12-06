58fc689e2d372904fb99892c5f53469dfcf06efc
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2023-12-06 09:47:40 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,22 @@
 class Solution {
-    static int[] flag;
-    static int max=0;
-    
+    static boolean[] flag;
+    static int max = 0;
+
+
     public int solution(int k, int[][] dungeons) {
-        flag = new int[dungeons.length];
+        flag = new boolean[dungeons.length];
         dfs(k, dungeons, 1);
 
         return max;
     }
-    
+
     public static void dfs(int tired, int[][] dungeons, int cnt) {
 
         for (int i = 0; i < dungeons.length; i++) {
-            if (flag[i] == 0 && tired >= dungeons[i][0]) {
-                flag[i] = 1;
+            if (!flag[i] && tired >= dungeons[i][0]) {
+                flag[i] = true;
                 dfs(tired - dungeons[i][1], dungeons, cnt+1);
-                flag[i] = 0;
+                flag[i] = false;
                 if(max < cnt) max = cnt;
             }
         }

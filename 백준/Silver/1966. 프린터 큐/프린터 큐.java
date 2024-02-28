89defec7f3fe283ee9f8c5d739f566765aa37ab7
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-02-28 14:05:34 +0900

    [Silver III] Title: 프린터 큐, Time: 184 ms, Memory: 27744 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Element {
+        int value;
+        boolean isTarget = false;
+    }
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+
+    static Queue<Element> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int total = stoi(st.nextToken());
+            int target = stoi(st.nextToken());
+            int answer = 1;
+
+            st = new StringTokenizer(br.readLine());
+            queue = new LinkedList<>();
+            for (int j = 0; j < total; j++) {
+                Element element = new Element();
+                if(j == target) element.isTarget = true;
+                element.value = stoi(st.nextToken());
+                queue.add(element);
+            }
+
+            while(!queue.isEmpty()){
+                if(!checkImportance()){
+                    queue.add(queue.poll());
+                }
+                else {
+                    Element element = queue.poll();
+                    if(element.isTarget){
+                        System.out.println(answer);
+                        break;
+                    }
+                    else answer++;
+                }
+            }
+        }
+    }
+
+    public static boolean checkImportance() {
+        Queue<Element> copied = new LinkedList<>(queue);
+        int a = copied.poll().value;
+
+        while(!copied.isEmpty()) {
+            if(a < copied.poll().value){
+                return false;
+            }
+        }
+        return true;
+    }
+}

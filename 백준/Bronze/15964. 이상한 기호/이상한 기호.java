6544df3cf1c0233179bf178f4a9423203c50b93c
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-03-10 23:21:34 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14204 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = stoi(st.nextToken());
+        long B = stoi(st.nextToken());
+
+        System.out.println((A+B)*(A-B));
+    }
+}

6c0ce273c567c204a722e38064668217da0ba3ff
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-03-17 18:39:39 +0900

    [Silver I] Title: 최대공약수, Time: 312 ms, Memory: 38744 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long stoi(String s) {
+        return Long.parseLong(s);
+    }
+
+    static long A;
+    static long B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = stoi(st.nextToken());
+        B = stoi(st.nextToken());
+
+        long c = gcd(A, B);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < c; i++) {
+            sb.append("1");
+        }
+        System.out.println(sb);
+    }
+
+    public static long gcd(long a, long b) {
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+}

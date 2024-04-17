90d14299566b3b5bec681fd5da2857fd0a56538d
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-04-17 12:59:53 +0900

    [Gold IV] Title: 테트로미노, Time: 1660 ms, Memory: 303708 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -8,7 +8,7 @@ public class Main {
 
     static int N, M;
     static int[][] paper;
-    static int[][] visited;
+    static boolean[][] visited;
     static int answer = 0;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
@@ -21,7 +21,7 @@ public class Main {
         N = stoi(st.nextToken());
         M = stoi(st.nextToken());
         paper = new int[N][M];
-        visited = new int[N][M];
+        visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -32,11 +32,11 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                visited[i][j] = 1;
+                visited[i][j] = true;
                 history.add(new int[] {i, j});
                 dfs(i, j, 1, paper[i][j]);
                 history.pop();
-                visited[i][j] = 0;
+                visited[i][j] = false;
             }
         }
 
@@ -54,12 +54,12 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny] == 0) {
-                visited[nx][ny] = 1;
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]) {
+                visited[nx][ny] = true;
                 history.add(new int[] {nx, ny});
                 dfs(nx, ny, num+1, sum + paper[nx][ny]);
                 history.pop();
-                visited[nx][ny] = 0;
+                visited[nx][ny] = false;
             }
         }
 
@@ -76,7 +76,7 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int nx = a[1] + dx[i];
                 int ny = b[1] + dy[i];
-                if(nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny] == 0) {
+                if(nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]) {
                     history.add(new int[] {nx, ny});
                     dfs(nx, ny, num+1, sum + paper[nx][ny]);
                     history.pop();

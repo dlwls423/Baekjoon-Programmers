dbd4f2140bfcda9709397751ac4ce7899871d180
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-05-28 13:53:27 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 440 ms, Memory: 295408 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+    static int[][] room;
+    static int way = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        room = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                room[i][j] = stoi(st.nextToken());
+            }
+        }
+
+        Pipe pipe = new Pipe(new int[] {0, 1}, Shape.ROW);
+
+        dfs(pipe);
+
+        System.out.println(way);
+    }
+
+    public static void dfs(Pipe pipe) {
+        if(pipe.right[0] == N-1 && pipe.right[1] == N-1) {
+            way++;
+            return;
+        }
+
+        // 가로로 이동
+        if(pipe.shape == Shape.ROW || pipe.shape == Shape.DIAGONAL) {
+            if(pipe.right[1]+1 < N && room[pipe.right[0]][pipe.right[1]+1] == 0) dfs(new Pipe(new int[] {pipe.right[0], pipe.right[1]+1}, Shape.ROW));
+        }
+
+        // 세로로 이동
+        if(pipe.shape == Shape.COL || pipe.shape == Shape.DIAGONAL) {
+            if(pipe.right[0]+1 < N && room[pipe.right[0]+1][pipe.right[1]] == 0) dfs(new Pipe(new int[] {pipe.right[0]+1, pipe.right[1]}, Shape.COL));
+        }
+
+        // 대각선으로 이동
+        if(pipe.right[0]+1 < N && pipe.right[1]+1 < N && room[pipe.right[0]+1][pipe.right[1]] == 0 && room[pipe.right[0]][pipe.right[1]+1] == 0 && room[pipe.right[0]+1][pipe.right[1]+1] == 0) {
+            dfs(new Pipe(new int[] {pipe.right[0]+1, pipe.right[1]+1}, Shape.DIAGONAL));
+        }
+    }
+
+    public enum Shape {
+        ROW, COL, DIAGONAL
+    }
+
+    public static class Pipe {
+        int[] right;
+        Shape shape;
+
+        public Pipe(int[] right, Shape shape) {
+            this.right = right;
+            this.shape = shape;
+        }
+    }
+}

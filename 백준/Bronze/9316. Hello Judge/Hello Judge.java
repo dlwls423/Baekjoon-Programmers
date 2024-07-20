2a199381c248f86e5aeabcadc5a1da3f1826a1d3
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-07-20 21:24:58 +0900

    [Bronze IV] Title: Hello Judge, Time: 184 ms, Memory: 15260 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            System.out.printf("Hello World, Judge %d!", i+1);
+            if(i != N-1) System.out.printf("\n");
+        }
+    }
+}

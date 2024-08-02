80613a27655a1227d421ae6e7cfd9df7b587fa18
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-08-02 17:09:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 576 ms, Memory: 47740 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Edge implements Comparable<Edge> {
+        int a;
+        int b;
+        int w;
+
+        public Edge(int a, int b, int w) {
+            this.a = a;
+            this.b = b;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return w - o.w;
+        }
+    }
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int V, E;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = stoi(st.nextToken());
+        E = stoi(st.nextToken());
+
+        parent = new int[V+1];
+
+        for (int i = 0; i < V; i++) {
+            parent[i+1] = i+1;
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = stoi(st.nextToken());
+            int b = stoi(st.nextToken());
+            int c = stoi(st.nextToken());
+
+            pq.add(new Edge(a, b, c));
+        }
+
+        int result = 0;
+        while(!pq.isEmpty()) {
+            Edge edge = pq.poll();
+
+            int a = edge.a;
+            int b = edge.b;
+            if(findParent(a) != findParent(b)) {
+                union(a, b);
+                result += edge.w;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static int findParent(int n) {
+        if(parent[n] == n) return n;
+        return findParent(parent[n]);
+    }
+
+    public static void union(int a, int b) {
+        int rootA = findParent(a);
+        int rootB = findParent(b);
+        
+        if(rootA < rootB) {
+            parent[rootB] = rootA;
+        }
+        else {
+            parent[rootA] = rootB;
+        }
+    }
+}

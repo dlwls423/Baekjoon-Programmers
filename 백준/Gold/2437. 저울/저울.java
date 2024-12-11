52d61c9bd3e363c91fba442bde3ea0029ed06ba2
LeeYejin <99391320+dlwls423@users.noreply.github.com>
2024-12-11 15:54:29 +0900

    [Gold II] Title: 저울, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = stoi(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 1;
+        for (int i = 0; i < N; i++) {
+            if(answer < arr[i]) break;
+            answer += arr[i];
+        }
+
+        System.out.println(answer);
+    }
+}
